f8ff74f508f62a07e1f79c7ed9818c33ba61a2e8
PollyGotACracker <bitterns96@gmail.com>
2022-10-12 11:01:36 +0900

    js 시작하기

---
+++ helloJS/main.js
@@ -0,0 +1,35 @@
+document.write("<p>우리나라만세")
+document.write("<h1>대한민국만세</h1>	")
+document.write("우리나라만세")
+document.write("가")
+document.write("나")
+document.write("다")
+document.write("라")
+document.write("<p>")
+document.write(30)
+document.write("<p>")
+document.write("30")
+document.write("<p>")
+document.write(30 + 40)
+document.write("<p>")
+document.write("30 + 40")
+
+/**
+ * 여기는 설명문(Comment, Remark) 
+ * 여기에 작성된 text는 해석하지 않고
+ * 무시한다.
+ * 개발자가 참조하기 위해 작성하는 부분이다.
+ * 
+ * const 우리나라만세 = "Republic of Korea"
+ * 1. 기억장소를 하나 비워달라 : const
+ * 2. 그 기억장소에 문자열("") Republic of Korea를 저장해달라
+ * 3. 그리고 그 기억장소에 우리나라만세 라는 이름을 붙여달라
+ * 이 절차를 변수의 선언(Varriable Define)
+ */
+const 우리나라만세 = "Republic of Korea"
+
+/**
+ * 어딘가에 있을 기억장소 이름 우리나라만세를 찾아서
+ * 그 곳에 저장된 내용을 document에 write 하라
+ */
+document.write(우리나라만세)
